dc1dd007a7e834632881717cfec4e735d954bce0
Jeremy Hansma <hansmajeremy@gmail.com>
2022-06-30 14:55:03 -0700

    Final Touches to basic JS Game to 5

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 function computerPlay(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
     let computerSelection = "";
-    let result = function (min, max) { Math.floor(Math.random() * (max - min)) + min;}
+    let result = Math.floor(Math.random() * 4) + min;
 
-    if(result = 1){
+    if(result === 1 ){
         computerSelection = "Rock";
-    } else if (result = 2){
+    } else if (result === 2){
         computerSelection = "Paper";
     } else {
         computerSelection = "Scissors";
     }
-     return computerSelection;
+    
+    return computerSelection;
 }
-let computerSelection = computerPlay(1, 3);
-console.log(computerSelection);
 
 
-let playerSelection_nocase = prompt("Your choice? ", " ");
-let playerSelection = playerSelection_nocase.toLowerCase(); //changes the players input to lowercase variable. 
-console.log(playerSelection);
-//alert(computerSelection);
-//alert(playerSelection);
+function askPlayer(){
+    let playerSelection_nocase = prompt("Your choice? ", " ");
+    let playerSelection = playerSelection_nocase.toLowerCase();
+    return playerSelection;
+}
 
-function playRound(playerSelection, computerSelection) {
+
+function playRound() {
+    let playerSelection = askPlayer();
+    let computerSelection = computerPlay(1, 3);
     let result = "";
+
     if (playerSelection === "rock" && computerSelection === "Paper"){
         result = "You Lose! Paper beats Rock";
     } else if (playerSelection === "rock" && computerSelection === "Scissors"){
@@ -36,11 +41,39 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         result = "You Win! Scissors beats Paper";
     } else {
-        result = "Tie!";
+        result = "It's a Tie!";
     }
-    return result;
+    console.log(result);
+    let resultCode = result.slice(0,7);
+    return resultCode;
 }
 
-let roundResult = playRound(playerSelection, computerSelection);
+function game(){
+    let userScore = 0
+    let winner = "";
+    
+    for(i = 0; i < 5; i++){
+        let resultCode = playRound();
+        
+        if(resultCode === "You Win"){
+            userScore += 1;
+        } else if(resultCode === "You Los") {
+            userScore -= 1;
+        } else {
+            userScore = userScore;
+            i = i - 1;
+        }
+    }
+    
+    alert(userScore);
+    if (userScore >= 3){
+        winner = "You are the winner!!";
+    } else {
+        winner = "You have lost to technology, how embarrassing";
+    }
 
-alert(roundResult);
+    return winner;
+}
+   
+winner = game();
+alert(winner);
